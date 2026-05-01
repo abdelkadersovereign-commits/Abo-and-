4bb91718978ed8ca12884ff50b0cc17b595ee0d0
Abouda <abdelkader.sovereign@gmail.com>
2026-05-01 11:17:28 +0300

    fix: إضافة مكوّن google-services للمشروع

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
